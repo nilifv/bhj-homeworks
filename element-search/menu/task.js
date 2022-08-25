7b691591bba4bf370086f7d8fcf16b8c493042e4
Виктор Филин <filinn14@gmail.com>
2022-08-25 18:45:30 +0300

    Меню сделал, слайдер не понимаю как сделать через findIndex

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,8 +1,13 @@
-const menuLink = document.getElementsByClassName('menu__link')
-
+let menuLink = document.getElementsByClassName("menu__link");
 for (let item of menuLink) {
-    item.onclick = function () {
-        item.closest(".menu__item").querySelector(".menu_sub").className = "menu menu_sub menu_active";
-        return false
+   item.onclick = function () {
+        if(item.closest(".menu__item").querySelector(".menu_sub.menu_active")) {
+            item.closest(".menu__item").querySelector(".menu_sub.menu_active").className = "menu menu_sub";
+            return false;
+        } else if(item.closest(".menu__item").querySelector(".menu_sub")) {
+            item.closest(".menu__item").querySelector(".menu_sub").className = "menu menu_sub menu_active"
+            return false;
+        }
     }
-}
+ }
+ 
