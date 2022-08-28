17e47708bee1997587c46c85768b4c3cea0eba16
Виктор Филин <filinn14@gmail.com>
2022-08-28 20:19:57 +0300

    Исправил, вроде работает, не на до конца разобрался

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,8 +1,8 @@
-const dropwdownValue = document.querySelector('.dropdown__value')
+const dropdownValue = document.querySelector('.dropdown__value')
 const dropdownList = document.querySelector('.dropdown__list')
 const dropdownItem = Array.from(document.querySelectorAll('.dropdown__item'))
 
-dropwdownValue.addEventListener('click', function() {
+dropdownValue.addEventListener('click', function() {
     if (dropdownList.className == 'dropdown__list dropdown__list_active') {
         dropdownList.className = 'dropdown__list'
     } else {
@@ -10,9 +10,10 @@ dropwdownValue.addEventListener('click', function() {
     }
 })
 
-for (item of dropdownItem) {
-    item.addEventListener('click', function(e) {
-        e.preventDefault()
-        dropwdownValue.textContent = item.textContent
+for (let item of dropdownItem) {
+    item.addEventListener("click", function(e) {
+        e.preventDefault();
+        dropdownValue.textContent = dropdownItem[dropdownItem.indexOf(item)].querySelector(".dropdown__link").textContent;
+        dropdownList.className = "dropdown__list";
     })
 }
