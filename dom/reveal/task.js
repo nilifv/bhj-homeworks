585667a5b076f9911f379d594c66f7ee03c6ed79
Виктор Филин <filinn14@gmail.com>
2022-08-31 09:02:22 +0300

    Исправил reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,13 +2,13 @@ const reveal = document.querySelectorAll('.reveal')
 
 for (let item of reveal) {
     window.addEventListener('scroll', () => {
-        let windowHeight = window.height;
+        let windowHeight = window.innerHeight;
         let blockTop = item.getBoundingClientRect().top;
 
         if (blockTop < windowHeight && blockTop > 0) {
-            item.classList.remove('reveal_active')
-        } else {
             item.classList.add('reveal_active')
+        } else {
+            item.classList.remove('reveal_active')
         }
     });
 }
